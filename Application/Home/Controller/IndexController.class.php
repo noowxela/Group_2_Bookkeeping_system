f4bad603a47f62b4dd0f97ec5a735a738d01772b
noowxela <noowxela@gmail.com>
2020-09-16 22:37:39 +0800

    fix the algo bug

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -23,17 +23,30 @@ class IndexController extends BaseController {
         $transaction_data = $ListData['data'];
         $sorted_data = [];
         $same_date_data = [];
-        $temp = '';
-        $counter = -1;
+        // $temp = '';
+        $counter = 0;
         $daily_data= [];
         $daily_spend_income = [];
 
-        foreach($transaction_data as $value) {
+        foreach($transaction_data as $k=>$value) {
             // var_dump($value) ;
+            // echo($value['money']) ;
             // echo ('<br>');
-            if($value['time'] != $temp){
 
-                $counter+=1;
+            if($value['time'] != $transaction_data[$k-1]['time'] ){
+                // echo('  -------        ') ;
+                // echo($value['money']) ;
+                // echo('  -------        ') ;
+                // echo($value['time']) ;
+                
+                // echo ('<br>');
+                // echo ($k);
+                // echo ('<br>');
+
+
+
+                // $temp = $value['time'];
+
                 if($counter != 0){
                     $sum_income =0;
                     $sum_spend  =0;
@@ -48,16 +61,63 @@ class IndexController extends BaseController {
 
                     array_push($daily_data,['income'=> number_format($sum_income,2) ,'expenses'=>number_format($sum_spend,2)]);
                     array_push($sorted_data,$same_date_data);
+                    $same_date_data = [];
+
+                } 
+                array_push($same_date_data,$value);
+                $counter+=1;
+
+                /// if is last index but is same time then push
+                if($k == count($transaction_data)-1){
+                    if($counter != 0){
+                        $sum_income =0;
+                        $sum_spend  =0;
+                        foreach($same_date_data as $v){
+                            if($v['typeid']==1){
+                                $sum_income+=$v['money'];
+                            }elseif($v['typeid']==2){
+                                $sum_spend+=$v['money'];
+                            }
+                            
+                        }
+    
+                        array_push($daily_data,['income'=> number_format($sum_income,2) ,'expenses'=>number_format($sum_spend,2)]);
+                        array_push($sorted_data,$same_date_data);
+                        $same_date_data = [];
+    
+                    } 
                 } 
 
-                $same_date_data = [];
-                $temp = $value['time'];
-                // print_r($counter);
-                array_push($same_date_data,$value);
 
-            }else{
-                array_push($same_date_data,$value);
+            }
+            /// if is last index but is same time then push
+            else if($k == count($transaction_data)){
+                // echo ('<br>');
+                // echo ($k);
+                // echo ('<br>');
+                if($counter != 0){
+                    $sum_income =0;
+                    $sum_spend  =0;
+                    foreach($same_date_data as $v){
+                        if($v['typeid']==1){
+                            $sum_income+=$v['money'];
+                        }elseif($v['typeid']==2){
+                            $sum_spend+=$v['money'];
+                        }
+                        
+                    }
 
+                    array_push($daily_data,['income'=> number_format($sum_income,2) ,'expenses'=>number_format($sum_spend,2)]);
+                    array_push($sorted_data,$same_date_data);
+                    $same_date_data = [];
+
+                } 
+            }
+            else{
+                // echo ('<br>');
+                // echo ($k);
+                // echo ('<br>');
+                array_push($same_date_data,$value);
 
             }
 
@@ -68,6 +128,12 @@ class IndexController extends BaseController {
         // echo ('<br>');
 
         // foreach($sorted_data as $value) {
+        //     var_dump($value) ;
+        //     echo ('<br>');
+        //     echo ('<br>');
+        // }    
+        // echo('<br>hello <br>');
+        // foreach($ListData['data'] as $value) {
         //     var_dump($value) ;
         //     echo ('<br>');
         //     echo ('<br>');
